4b1dc68e32fd362d24cea6c48ee0a41a152a02ef
ioksing <g9893006@gmail.com>
2024-07-24 12:55:01 +0800

    add comment

--- yaml2ini.py
+++ yaml2ini.py
@@ -5,26 +5,26 @@ import yaml
 
 
 def yaml_to_ini(yaml_file, ini_file):
-    # 讀取YAML文件
+
+    # Read the YAML file
     with open(yaml_file, 'r') as file:
         yaml_data = yaml.safe_load(file)
 
-    # 創建ConfigParser對象
+    # Create a ConfigParse object
     config = ConfigParser()
 
-    # 將YAML數據轉換為INI格式
+    # Convert YAML format data to INI format.
     for section, values in yaml_data.items():
         config.add_section(section)
         for key, value in values.items():
             config.set(section, key, str(value))
 
-    # 將INI數據寫入文件
     with open(ini_file, 'w') as file:
         config.write(file)
 
 
 if __name__ == "__main__":
-    yaml_file = 'passwd.yml'
+    yaml_file = 'input.yaml'
     ini_file = 'output.ini'
     yaml_to_ini(yaml_file, ini_file)
     yaml_to_ini(yaml_file, ini_file)
